3afae99b410d1a571ece51633447204231a9e9cc
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-05-20 12:10:46 +0900

    push

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,33 @@
-import express from 'express';
-import room from '../controllers/room.js'
+import express from "express";
+import room from "../controllers/room.js";
 var router = express.Router();
 
-router.post('/api/createRoom', (req,res) => {
-    Promise.all([room.insert()])
-    .then((result)=>{
-        req.session.save((err) => {
-            if(!err) res.send(result);
-        })
-        
-    })
+router.post("/api/createRoom", (req, res) => {
+  Promise.all([room.insert()]).then((result) => {
+    req.session.save((err) => {
+      if (!err) res.send(result);
+    });
+  });
 });
 
-router.get('/api/getRoom/:roomId', (req, res) => {
-    console.log("trying to get room" + req.params.roomId);
-    Promise.all([room.getById(req.params.roomId)])
-    .then((result)=>{
-        req.session.save((err) => {
-            if(!err) res.send(result[0].id);
-        })
-        //alternatively: res.send(result);
-    }).catch(err=>{ 
-       res.send(null);
-    }) 
-   
+router.get("/api/getRoom/:roomId", (req, res) => {
+  console.log("trying to get room" + req.params.roomId);
+  Promise.all([room.getById(req.params.roomId)])
+    .then((result) => {
+      req.session.save((err) => {
+        if (!err) res.send(result[0].id);
+      });
+      //alternatively: res.send(result);
+    })
+    .catch((err) => {
+      res.send(null);
+    });
 });
+async function dd(req, res) {
+  const payload = "test is success!";
+  res.send(payload);
+}
+
+router.post("/test-section", dd);
 
 export default router;
